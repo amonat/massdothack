c7e6f9d5381b1cabd643a1799e84b1ede74286f9
Andrew Collier <andrew@acollier.com>
2013-12-14 13:18:16 -0500

    added overlay

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ var map;
 function initialize() {
   var mapOptions = {
     center: new google.maps.LatLng(42.358056, -71.063611),
-    zoom: 8
+    zoom: 9
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 
@@ -182,3 +182,55 @@ function initialize() {
   }
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+ nv.addGraph(function() {  
+   var chart = nv.models.lineChart();
+ 
+   chart.xAxis
+       .axisLabel('Time (ms)')
+       .tickFormat(d3.format(',r'));
+ 
+   chart.yAxis
+       .axisLabel('Voltage (v)')
+       .tickFormat(d3.format('.02f'));
+ 
+   d3.select('#chart svg')
+       .datum(sinAndCos())
+     .transition().duration(500)
+       .call(chart);
+ 
+   nv.utils.windowResize(function() { d3.select('#chart svg').call(chart) });
+ 
+   return chart;
+ });
+
+
+
+
+ /**************************************
+  * Simple test data generator
+  */
+
+
+ function sinAndCos() {
+   var sin = [],
+       cos = [];
+ 
+   for (var i = 0; i < 100; i++) {
+     sin.push({x: i, y: Math.sin(i/10)});
+     cos.push({x: i, y: .5 * Math.cos(i/10)});
+   }
+ 
+   return [
+     {
+       values: sin,
+       key: 'Sine Wave',
+       color: '#ff7f0e'
+     },
+     {
+       values: cos,
+       key: 'Cosine Wave',
+       color: '#2ca02c'
+     }
+   ];
+}
